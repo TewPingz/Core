061697df80b481cf54f52f35eb2a158c9f8d4d41
TewPingz <38973872+TewPingz@users.noreply.github.com>
2022-11-10 21:10:04 +1100

    Made sure to remove the player if their login is cancelled

--- core-bukkit/src/main/java/me/tewpingz/core/profile/ProfileListener.java
+++ core-bukkit/src/main/java/me/tewpingz/core/profile/ProfileListener.java
@@ -3,6 +3,7 @@ package me.tewpingz.core.profile;
 import lombok.RequiredArgsConstructor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -14,7 +15,7 @@ public class ProfileListener implements Listener {
 
     private final ProfileManager profileManager;
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.LOWEST)
     public void onAsyncPlayerPreLogin(AsyncPlayerPreLoginEvent event) {
         UUID uuid = event.getUniqueId();
 
@@ -24,6 +25,14 @@ public class ProfileListener implements Listener {
         });
 
         this.profileManager.beginCachingLocally(uuid);
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onAsyncPlayerPreLoginMonitor(AsyncPlayerPreLoginEvent event) {
+        if (event.getLoginResult() != AsyncPlayerPreLoginEvent.Result.ALLOWED) {
+            // Make sure to unload the profile from cache if the login is cancelled.
+            this.profileManager.stopCachingLocally(event.getUniqueId());
+        }
     }
 
     @EventHandler
